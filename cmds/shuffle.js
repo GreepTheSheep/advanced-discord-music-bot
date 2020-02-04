3c81c60766acdb0a2fa4ff494e8cf9298d8624e6
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-02-05 00:33:19 +0100

    Update shuffle.js

--- cmds/shuffle.js
+++ cmds/shuffle.js
@@ -15,9 +15,11 @@ function shuffle(a) {
 function shucmd(message, client, serverQueue) {
 	if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
     if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+    console.log(serverQueue.songs)
     var queueToShuffle = serverQueue.songs.splice(0,1)
     var queueShuffled = shuffle(queueToShuffle)
     var newQueue = serverQueue.songs.splice(1).push(queueShuffled)
+    console.log(newQueue)
     serverQueue.songs = newQueue
     message.react('🔀')
 }
