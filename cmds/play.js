ef31d5956a7382b5c6a2e766d2ac0f0300835350
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-03-24 17:42:44 +0100

    Update play.js

--- cmds/play.js
+++ cmds/play.js
@@ -15,16 +15,16 @@ function play(guild, song, queue) {
 		.on('end', () => {
 			console.log(`${song.title} ended in ${guild.name}`)
 			if (serverQueue.loop == false) serverQueue.songs.shift();
-                        if (serverQueue.shuffle == true) {
-                            if (serverQueue.loop == true) console.log(`${guild.name} - Shuffle actived, but loop actived: doing nothing.`)
-                            else if (serverQueue.loop == false) {
-                                let random = Math.floor(Math.random() * serverQueue.songs.length)
-                                var newFirst = serverQueue.songs[random]
-                                    serverQueue.songs.splice(newFirst, 1);
+            if (serverQueue.shuffle == true) {
+                if (serverQueue.loop == true) console.log(`${guild.name} - Shuffle actived, but loop actived: doing nothing.`)
+                else if (serverQueue.loop == false) {
+                    let random = Math.floor(Math.random() * serverQueue.songs.length)
+                    var newFirst = serverQueue.songs[random]
+                    serverQueue.songs.splice(newFirst, 1);
 				    serverQueue.songs.unshift(newFirst)
-                            }
-                        }
-                        play(guild, serverQueue.songs[0], queue);
+                }
+            }
+            play(guild, serverQueue.songs[0], queue);
 		})
 		.on('error', error => {
 			console.error(error);
